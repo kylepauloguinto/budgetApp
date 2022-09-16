25a1b8abb3512f1eb5e8a991b93f399e8cb4aa7f
guintokylep <guintokyle01@gmail.com>
2022-09-16 21:56:38 +0900

    Schedule model adding

--- budgetApp/admin.py
+++ budgetApp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User, Categories, SubCategories, Account, Transaction, Budget
+from .models import User, Categories, SubCategories, Account, Transaction, Budget, Schedule
 
 class AccountAdmin(admin.ModelAdmin):
     list_display = ("id","userAccount", "accountName", "description","balance","previousBalance","read","ins_date")
@@ -12,11 +12,13 @@ class SubCategoriesAdmin(admin.ModelAdmin):
     list_display = ("id","userSubCategory", "parentCategory", "subCategory" ,"ins_date" )
     
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ("id","userTransaction", "accountNameTransaction", "accountNameTransferFrom", "accountNameTransferTo", "transactionFromId", "transactionType", "amount","previousAccountBalance","currentAccountBalance","descriptionTransaction","categoryTransaction", "subCategoryTransaction", "transactionDate", "readTransaction","ins_date")
+    list_display = ("id","userTransaction", "accountNameTransaction", "accountNameTransferFrom", "accountNameTransferTo", "transactionFromId", "transactionType", "amount","previousAccountBalance","currentAccountBalance","descriptionTransaction","scheduleId","categoryTransaction", "subCategoryTransaction", "transactionDate", "readTransaction","ins_date")
 
 class BudgetAdmin(admin.ModelAdmin):
     list_display = ("id","userBudget", "budgetName", "accountNameBudget", "currentAmount", "budgetAmount", "descriptionBudget", "categoryBudget","subCategoryBudget","startDate","endDate", "periodCount", "periodProcess", "minusAmount","ins_date")
 
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ("id","userSchedule", "accountNameSchedule", "accountNameScheduleTransferFrom", "accountNameScheduleTransferTo", "scheduleFromId", "scheduleType", "amount","previousScheduleAccountBalance","currentScheduleAccountBalance","descriptionSchedule", "transactionId", "categorySchedule", "subCategorySchedule", "scheduleDate","repeatSchedule","ins_date")
 
 admin.site.register(User)
 admin.site.register(Account, AccountAdmin)
@@ -24,3 +26,4 @@ admin.site.register(Categories, CategoriesAdmin)
 admin.site.register(SubCategories, SubCategoriesAdmin)
 admin.site.register(Transaction, TransactionAdmin)
 admin.site.register(Budget, BudgetAdmin)
+admin.site.register(Schedule, ScheduleAdmin)
